cded2005eb5626f42c3f639ce0dbe80465658062
Karol <karol.cuellarm@gmail.com>
2022-07-26 22:20:54 -0500

    agregar logo y menu

--- resources/views/layouts/src/header.blade.php
+++ resources/views/layouts/src/header.blade.php
@@ -5,10 +5,10 @@
             <div class="navbar-brand-box">
                 <a href="/" class="logo logo-dark">
                     <span class="logo-sm" >
-                        <img src="{{asset('assets/images/logo-sm-dark.png')}}" alt="" height="40">
+                        <img src="{{asset('assets/images/Logo2.png')}}" alt="" height=40 >
                     </span>
                     <span class="logo-lg">
-                        <img src="{{asset('assets/images/logo-dark.png')}}" alt="" height="70">
+                        <img src="{{asset('assets/images/Logo.png')}}" alt="" height=57 >
                     </span>
                 </a>
 
@@ -20,12 +20,12 @@
             </button>
 
             <!-- App Search-->
-            <form class="app-search d-none d-lg-block">
+            {{-- <form class="app-search d-none d-lg-block">
                 <div class="position-relative">
                     <input type="text" class="form-control" placeholder="Buscar...">
                     <span class="mdi mdi-magnify"></span>
                 </div>
-            </form>
+            </form> --}}
         </div>
 
         <div class="d-flex">
